eb5abce6349812c30a9742b8e988a1bae08a2f85
dienoov <alvinodien@yahoo.co.id>
2022-03-11 18:00:51 +0700

    deploy

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
 import Blockies from 'react-blockies';
 import { Component } from 'react';
+import Image from 'next/image';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import axios from 'axios';
+import { nftAddress, nftMarketAddress } from '../address';
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
+import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      cards: [],
+      nfts: [],
       skeletons: [],
       isLoading: true,
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const skeletons = [];
-    const cards = [];
 
     for (let i = 0; i < 8; i += 1) {
       skeletons.push((
@@ -31,53 +37,92 @@ export default class Home extends Component {
               </div>
               <div className="flex justify-between items-center mt-6">
                 <div className="rounded w-16 h-4 bg-neutral-600" />
-                <div className="rounded-full w-16 h-8 bg-neutral-600" />
+                <div className="rounded-full w-20 h-8 bg-neutral-600" />
               </div>
-            </div>
-          </div>
-        </div>
-      ));
-
-      cards.push((
-        <div className="rounded-2xl overflow-hidden bg-neutral-700" key={i}>
-          <div className="h-48 flex justify-center items-center overflow-hidden">
-            <Blockies seed={Math.random().toString()} scale={64} />
-          </div>
-          <div className="p-4">
-            <div className="flex justify-between items-center">
-              <h3 className="font-semibold">{Math.random().toString(36).substring(2)}</h3>
-              <Blockies seed={Math.random().toString()} className="rounded-full" />
-            </div>
-            <div className="flex justify-between items-center mt-6">
-              <span>{`${Math.random().toFixed(3)} ETH`}</span>
-              <button
-                type="button"
-                className="rounded-full border border-neutral-400 px-4 py-1 font-semibold
-                hover:bg-blue-600 hover:border-blue-600 transition"
-              >
-                Buy
-              </button>
             </div>
           </div>
         </div>
       ));
     }
 
-    this.setState({ cards, skeletons });
+    this.setState({ skeletons });
 
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 1000);
+    await this.loadNFTs();
+  }
+
+  async loadNFTs() {
+    this.setState({ isLoading: true });
+
+    const provider = new ethers.providers.JsonRpcProvider();
+    const nftContract = new ethers.Contract(nftAddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(nftMarketAddress, Market.abi, provider);
+
+    const data = await marketContract.fetchMarketItems();
+    const items = await Promise.all(data.map(async (item) => {
+      const tokenUri = await nftContract.tokenURI(item.tokenId);
+      const meta = await axios.get(tokenUri);
+      const price = ethers.utils.parseUnits(item.price.toString(), 'ether');
+
+      return {
+        price,
+        tokenId: item.tokenId.toNumber(),
+        seller: item.seller,
+        owner: item.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      };
+    }));
+
+    this.setState({ nfts: items, isLoading: false });
+  }
+
+  async buyNFT(nft) {
+    const web3Modal = new Web3Modal();
+    await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider();
+
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftMarketAddress, Market.abi, signer);
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether');
+
+    const transaction = await contract.createMarketSale(nftAddress, nft.tokenId, { value: price });
+    await transaction.wait();
+    this.loadNFTs();
   }
 
   render() {
-    const { cards, skeletons, isLoading } = this.state;
+    const { nfts, skeletons, isLoading } = this.state;
 
     return (
       <div className="px-3 py-6">
         <div className="w-full max-w-7xl mx-auto">
           <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {isLoading ? skeletons : cards}
+            {isLoading || !nfts.length ? skeletons : nfts.map((nft) => (
+              <div className="rounded-2xl overflow-hidden bg-neutral-700" key={nft.tokenId}>
+                <div className="h-48 flex justify-center items-center overflow-hidden">
+                  <Image src={nft.image} alt={nft.name} />
+                </div>
+                <div className="p-4">
+                  <div className="flex justify-between items-center">
+                    <h3 className="font-semibold">{nft.name}</h3>
+                    <Blockies seed={Math.random().toString()} className="rounded-full" />
+                  </div>
+                  <div className="flex justify-between items-center mt-6">
+                    <span>{`${nft.price} ETH`}</span>
+                    <button
+                      type="button"
+                      className="rounded-full border border-neutral-400 px-6 py-1 font-semibold
+                        hover:bg-blue-600 hover:border-blue-600 transition"
+                      onClick={this.buyNFT(nft)}
+                    >
+                      Buy
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
